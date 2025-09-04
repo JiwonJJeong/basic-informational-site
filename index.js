ce854634f84afcf9d6a70d4d281c870838bfd9f4
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-09-03 19:01:40 -0700

    FEAT: Read HTML files and send to browser
    Read file with fs
    
    Send to browser with response object.
    Specify status code and file type in res.writeHead.
    
    Actually put the data (read from file with fs)
    into res.end(...)

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 import http from 'node:http';
+import fs from 'fs';
 
 const server = http.createServer();
 
 server.on('request', (request, res) => {
+    let filePath;
+    let status = 200;
     switch (request.url){
         case '/':
-            console.log('a');
+            filePath='./index.html'
             break;
         case '/about':
-            console.log('b');
+            filePath='./about.html'
             break;
         case '/contact-me':
-            console.log('c');
+            filePath='./contact-me.html'
             break;
         default:
-            console.log('d');
-            console.log(request.url);
+            filePath='./404.html'
+            status = 404;
             break;
     } 
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err){
+            console.error('There was an error reading file:', err);
+            return;
+        }
+        res.writeHead(status, {
+            'Content-Type': 'text/html',
+        })
+        .end(data);
+    });
 })
 
 server.listen(8080);
